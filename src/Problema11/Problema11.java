413da1dc802cec1d690f16bea9c3970199b2ed96
LeoBruno400 <leobruno400@gmail.com>
2022-04-27 17:42:46 -0300

    comentarios problema11

--- src/Problema11/Problema11.java
+++ src/Problema11/Problema11.java
@@ -12,34 +12,40 @@ public class Problema11 {
     public static int sumaPosicionesPares(int[] arreglo, int i, int longitud) {
         int contadorPar = 0;
 
+        //Si la longitud es impar, se resta 1 para tomar hasta la ultima posicion par
         if (longitud % 2 != 0) {
             longitud -= 1;
         }
 
+        //Caso iterativo: mientras el i sea menor a la longitud
         if (i < longitud) {
             contadorPar = arreglo[i] + sumaPosicionesPares(arreglo, i + 2, longitud);
         } else {
-            contadorPar = arreglo[i];
+            contadorPar = arreglo[i]; 
+            //Caso base: contadorPar se va a setear con el ultimo valor par y se va a sumar con el acarreado
         }
         return contadorPar;
     }
     public static int sumaPosicionesImpares(int[] arreglo, int i, int longitud) {
-        int contadorPar = 0;
+        int contadorImpar = 0;
 
+        //Si la longitud es par, se resta 1 para tomar hasta la ultima posicion impar
         if (longitud % 2 == 0) {
             longitud -= 1;
         }
-
+        //Condicion impar: i tiene que arracar en la primera posicion impar del arreglo, osea 1
         if(i==0){
             i = 1;
         }
 
+        //Caso iterativo: mientras el i sea menor a la longitud
         if (i < longitud) {
-            contadorPar = arreglo[i] + sumaPosicionesImpares(arreglo, i + 2, longitud);
+            contadorImpar = arreglo[i] + sumaPosicionesImpares(arreglo, i + 2, longitud);
         } else {
-            contadorPar = arreglo[i];
+            contadorImpar = arreglo[i];
+            //Caso base: contadorImpar se va a setear con el ultimo valor impar y se va a sumar con el acarreado
         }
-        return contadorPar;
+        return contadorImpar;
     }
 
 
