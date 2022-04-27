d882e04622ad9de4fbcd026470e5924b6483a0a6
LeoBruno400 <leobruno400@gmail.com>
2022-04-27 15:05:55 -0300

    comentarios

--- src/Problema10/Problema10.java
+++ src/Problema10/Problema10.java
@@ -15,8 +15,10 @@ public class Problema10 {
         System.out.println("Ingrese una letra: \nPunto para terminar");
         caracterIngresado = sc.nextLine();
         if (caracterIngresado.equals(".")) {
+            //Caso base: si es "." caracter ingresado se setea con vacio para volver a sumar todos los caracteres acarreados
             caracterIngresado = " ";
         } else {
+            //Caso iterativo: que la palabra != de "."
             caracterIngresado += contarPalabra(nuevaCadena);
         }
         sc.close();
