843134bc950014591d7e29017d65a8832eae218b
LeoBruno400 <leobruno400@gmail.com>
2022-04-27 18:54:16 -0300

    estetica

--- src/Problema10/Problema10.java
+++ src/Problema10/Problema10.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Problema10 {
     public static void main(String[] args) {
         String cadena = " ";
-        System.out.println(contarPalabra(cadena));
+        System.out.println("La palabra formada es: " + contarPalabra(cadena));
     }
 
     public static String contarPalabra(String cadena) {
@@ -15,10 +15,11 @@ public class Problema10 {
         System.out.println("Ingrese una letra: \nPunto para terminar");
         caracterIngresado = sc.nextLine();
         if (caracterIngresado.equals(".")) {
-            //Caso base: si es "." caracter ingresado se setea con vacio para volver a sumar todos los caracteres acarreados
+            // Caso base: si es "." caracter ingresado se setea con vacio para volver a
+            // sumar todos los caracteres acarreados
             caracterIngresado = " ";
         } else {
-            //Caso iterativo: que la palabra != de "."
+            // Caso iterativo: que la palabra != de "."
             caracterIngresado += contarPalabra(nuevaCadena);
         }
         sc.close();
--- src/Problema11/Problema11.java
+++ src/Problema11/Problema11.java
@@ -5,45 +5,49 @@ public class Problema11 {
         int[] arreglo = { 2, 1, 2, 1, 2, 1 };
         int i = 0;
         int longitud = arreglo.length - 1;
-        System.out.println(sumaPosicionesPares(arreglo, i, longitud));
-        System.out.println(sumaPosicionesImpares(arreglo, i, longitud));
+        System.out.println("La suma de las posiciones pares son: " + sumaPosicionesPares(arreglo, i, longitud));
+        System.out.println("La suma de las posiciones impares son: " + sumaPosicionesImpares(arreglo, i, longitud));
     }
 
     public static int sumaPosicionesPares(int[] arreglo, int i, int longitud) {
         int contadorPar = 0;
 
-        //Si la longitud es impar, se resta 1 para tomar hasta la ultima posicion par
+        // Si la longitud es impar, se resta 1 para tomar hasta la ultima posicion par
         if (longitud % 2 != 0) {
             longitud -= 1;
         }
 
-        //Caso iterativo: mientras el i sea menor a la longitud
+        // Caso iterativo: mientras el i sea menor a la longitud
         if (i < longitud) {
             contadorPar = arreglo[i] + sumaPosicionesPares(arreglo, i + 2, longitud);
         } else {
-            contadorPar = arreglo[i]; 
-            //Caso base: contadorPar se va a setear con el ultimo valor par y se va a sumar con el acarreado
+            contadorPar = arreglo[i];
+            // Caso base: contadorPar se va a setear con el ultimo valor par y se va a sumar
+            // con el acarreado
         }
         return contadorPar;
     }
+
     public static int sumaPosicionesImpares(int[] arreglo, int i, int longitud) {
         int contadorImpar = 0;
 
-        //Si la longitud es par, se resta 1 para tomar hasta la ultima posicion impar
+        // Si la longitud es par, se resta 1 para tomar hasta la ultima posicion impar
         if (longitud % 2 == 0) {
             longitud -= 1;
         }
-        //Condicion impar: i tiene que arracar en la primera posicion impar del arreglo, osea 1
-        if(i==0){
+        // Condicion impar: i tiene que arracar en la primera posicion impar del
+        // arreglo, osea 1
+        if (i == 0) {
             i = 1;
         }
 
-        //Caso iterativo: mientras el i sea menor a la longitud
+        // Caso iterativo: mientras el i sea menor a la longitud
         if (i < longitud) {
             contadorImpar = arreglo[i] + sumaPosicionesImpares(arreglo, i + 2, longitud);
         } else {
             contadorImpar = arreglo[i];
-            //Caso base: contadorImpar se va a setear con el ultimo valor impar y se va a sumar con el acarreado
+            // Caso base: contadorImpar se va a setear con el ultimo valor impar y se va a
+            // sumar con el acarreado
         }
         return contadorImpar;
     }
