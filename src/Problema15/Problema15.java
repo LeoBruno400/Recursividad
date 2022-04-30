be707337f59572b557093800e45fcb508f45b17c
LeoBruno400 <leobruno400@gmail.com>
2022-04-30 18:24:59 -0300

    comentarios

--- src/Problema15/Problema15.java
+++ src/Problema15/Problema15.java
@@ -18,11 +18,13 @@ public class Problema15 {
 
     public static int vocalCadena(String cadena, int longitud) {
         int vocal = 0;
-        if (longitud >= 0) {
+        if (longitud >= 0) { //Mientras la longitud sea mayor o igual a 0 se sigue iterando
             if ((cadena.charAt(longitud) == 'a') || (cadena.charAt(longitud) == 'e') || (cadena.charAt(longitud) == 'i')
-                    || (cadena.charAt(longitud) == 'o') || (cadena.charAt(longitud) == 'u')) {
+                    || (cadena.charAt(longitud) == 'o') || (cadena.charAt(longitud) == 'u')) { 
+                        //Si la cadena tiene alguna vocal suma 1
                 vocal = 1 + vocalCadena(cadena, longitud - 1);
             }else{
+                //Si la cadena no encontro alguna vocal, hace lo mismo pero sin suma 1
                 vocal = vocalCadena(cadena, longitud - 1);
             }
         }
