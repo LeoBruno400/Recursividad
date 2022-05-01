0d442914517dced0870789af7908626cb360c2e8
LeoBruno400 <leobruno400@gmail.com>
2022-05-01 00:43:20 -0300

    comentarios

--- src/Problema17/Problema17.java
+++ src/Problema17/Problema17.java
@@ -6,16 +6,19 @@ public class Problema17 {
         piramideNumerosDecrecientes(numero);
     }
 
-    public static String piramideNumerosDecrecientes(int num){
-        String numIngresado = Integer.toString(num);
+    public static String piramideNumerosDecrecientes(int num) {
+        String numIngresado = Integer.toString(num); // Convierto el numero en una cadena de caracteres
+        // Caso Base: si el numero es igual a 1, suma un vacio y imprime 1
         if (numIngresado.equals("1")) {
             numIngresado += " ";
             System.out.println(numIngresado);
         } else {
-            numIngresado += piramideNumerosDecrecientes(Integer. parseInt(numIngresado)-1);
+            // Caso iterativo: convierto la cadena en int y le resto 1
+            // Esto lo hago asi para ir sumandolos en la variable nueva de tipo
+            // string(numIngresado)
+            numIngresado += piramideNumerosDecrecientes(Integer.parseInt(numIngresado) - 1);
             System.out.println(numIngresado);
         }
         return numIngresado;
     }
 }
-
