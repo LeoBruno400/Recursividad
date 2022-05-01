45197736729aa871c0458a0d6d114d990d32d828
LeoBruno400 <leobruno400@gmail.com>
2022-05-01 03:58:07 -0300

    problema18 a casovich

--- src/Problema18/Problema18.java
+++ src/Problema18/Problema18.java
@@ -1,5 +1,18 @@
 package Problema18;
 
 public class Problema18 {
-    
+    public static void main(String[] args) {
+        int N = 15;
+        int B = 3;
+        explotar(N, B);
+    }
+
+    public static void explotar(int N, int B) {
+        if (N <= B) {
+            System.out.println(N);
+        } else {
+            explotar((N / B), B);
+            explotar(N - (N / B), B);
+        }
+    }
 }
