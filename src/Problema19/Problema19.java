d19046ccc2d1bca23d1f90ac0d1796672950ee77
LeoBruno400 <leobruno400@gmail.com>
2022-05-02 02:25:16 -0300

    problema19 a casovich gracias maru <3

--- src/Problema19/Problema19.java
+++ src/Problema19/Problema19.java
@@ -3,7 +3,25 @@ package Problema19;
 public class Problema19 {
     public static void main(String[] args) {
         int numero = 9;
-        piramideNumerosDecrecientes(numero);
+        int espacios = 10;
+        System.out.println("--------------------------------------------------------");
+        piramideNumeros(numero, espacios);
+        System.out.println("--------------------------------------------------------");
+    }
+
+    public static String piramideNumeros(int num, int espacios) {
+        String numIngresado = Integer.toString(num);
+        String cadenaIterativa = "";
+
+        if (numIngresado.equals("1")) {
+            System.out.println(concatenaEspacios(espacios) + numIngresado);
+        } else {
+            numIngresado += " " + piramideNumeros(num - 1, espacios + 2);
+            System.out.print(concatenaEspacios(espacios));
+            cadenaIterativa += piramideNumerosIzquierda(num - 1) + " ";
+            System.out.println(cadenaIterativa + numIngresado);
+        }
+        return numIngresado;
     }
 
     public static String piramideNumerosIzquierda(int num) {
@@ -20,18 +38,12 @@ public class Problema19 {
         return numero;
     }
 
-    public static String piramideNumerosDecrecientes(int num) {
-        String numIngresado = Integer.toString(num);
-        String cadenaIterativa = "";
-
-        if (numIngresado.equals("1")) {
-            System.out.println(numIngresado);
-        } else {
-            numIngresado += " " + piramideNumerosDecrecientes(num - 1);
-            cadenaIterativa += piramideNumerosIzquierda(num - 1) + " ";
-            System.out.println(cadenaIterativa + numIngresado);
+    public static String concatenaEspacios(int espacios) {
+        String cantEspacios = "";
+        if (espacios > 0) {
+            cantEspacios += " " + concatenaEspacios(espacios - 1);
         }
-        return numIngresado;
+        return cantEspacios;
     }
 
 }
